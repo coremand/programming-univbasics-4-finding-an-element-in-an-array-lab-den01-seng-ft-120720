289d4bb647e8a6c98a3f8b106c420fc917a289c1
Obinna Nwabia <valinevite@gmail.com>
2020-11-12 06:00:44 +0000

    Automatically backed up by Learn

---
+++ lib/find_in_array.rb
@@ -0,0 +1,9 @@
+def find_element_index(array, value_to_find)
+  counter = 0
+  if counter < array.length && array.include?(value_to_find)
+      puts array.index(value_to_find)
+      counter += 1
+     else
+        puts "nil"
+  end
+end
